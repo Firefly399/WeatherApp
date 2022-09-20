3902ff1ecad5e849e4f057a01064195c8b19aafa
Firefly399 <111974778+Firefly399@users.noreply.github.com>
2022-09-20 16:45:53 +0200

    commit 1

---
+++ WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+    
+    var weatherManager = WeatherManager()
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+ 
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchTextField.endEditing(true)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = textField.text {
+        weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
+    }
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+            
+        }
+     
+    }
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+  
+}
+
---
+++ WeatherApp/Model/WeatherData.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by user226097 on 9/2/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    
+    
+    
+    
+    
+}
+
+struct Main: Codable {
+
+    let temp: Double
+}
+struct Weather: Codable {
+    
+    let id: Int
+}
